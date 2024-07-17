98245319d0663800c0b4c730871318bf4f7226ef
Anoop Reddy <anoop.reddy50@gmail.com>
2024-07-17 15:40:11 -0400

    JavaScript Basics | 📖

---
+++ JS/index.js
@@ -0,0 +1,7 @@
+console.log('Hello World');
+
+let name = 'Wampa';
+//To define variables in JS you use the word 'let'.
+//Shown above the way to define a variable would be to use let then the variable name and then the thing that is being stored inside the variable. 
+// There are some words that can't be used as a variable because JS will think you are trying to use something else. For example (if, else, and var) can not be used.
+console.log(name);
